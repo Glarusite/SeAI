334d41c2647566940655d771b5d13aa956705d66
Viktor Mitev <vmmitev@gmail.com>
2023-10-16 00:59:44 +0300

    added authorization token resolution from async storage

--- src/store/api.base.ts
+++ src/store/api.base.ts
@@ -1,8 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { getAsyncStorageState } from "./async-storage";
+
 const baseUrl = "http://ec2-18-194-242-209.eu-central-1.compute.amazonaws.com:8080/";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: async headers => {
+      const state = await getAsyncStorageState();
+      if (state?.user.accessToken) {
+        headers.set("authorization", `Bearer ${state.user.accessToken}`);
+      }
+    },
+  }),
   endpoints: () => ({}),
 });
