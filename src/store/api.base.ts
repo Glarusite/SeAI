7febf3e29b17edbaefed648594b4279822e1f505
Viktor Mitev <vmmitev@gmail.com>
2023-10-23 19:18:05 +0300

    optimized baseApi baseQuery with memoized factory

--- src/store/api.base.ts
+++ src/store/api.base.ts
@@ -1,37 +1,41 @@
-import type { FetchArgs } from "@reduxjs/toolkit/query/react";
+import type { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query/react";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { memoize } from "lodash";
 
-import type { AppStore, AppStoreState } from "./configure";
+import type { AppStoreState } from "./configure";
 import { setUser } from "./slices/user";
 
 const baseUrl = "http://ec2-18-194-242-209.eu-central-1.compute.amazonaws.com:8080/";
 
 export const baseApi = createApi({
   baseQuery: (queryParameters: string | FetchArgs, api, extraOptions) => {
-    const dispatch = api.dispatch as AppStore["dispatch"];
-    const appFetchQuery = fetchBaseQuery({
-      baseUrl,
-      responseHandler: async response => {
-        if (response.status === 403) {
-          dispatch(setUser({}));
-        }
-
-        const isEmptyResponse = Number(response.headers.get("content-length")) === 0;
-        if (response.status >= 400 || isEmptyResponse) {
-          return response.text();
-        }
-
-        return response.json();
-      },
-      prepareHeaders: (headers, { getState }) => {
-        const state = getState() as AppStoreState;
-        if (state?.user.accessToken) {
-          headers.set("Authorization", `Bearer ${state.user.accessToken}`);
-        }
-      },
-    });
-
-    return appFetchQuery(queryParameters, api, extraOptions);
+    const appFetchBaseQuery = createAppFetchBaseQuery(api.dispatch);
+    return appFetchBaseQuery(queryParameters, api, extraOptions);
   },
   endpoints: () => ({}),
 });
+
+const createAppFetchBaseQuery = memoize((dispatch: BaseQueryApi["dispatch"]) =>
+  fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as AppStoreState;
+      if (state?.user.accessToken) {
+        headers.set("Authorization", `Bearer ${state.user.accessToken}`);
+      }
+    },
+    responseHandler: async (response: Response) => {
+      const { status, headers } = response;
+      if (status === 401 || status === 403) {
+        dispatch(setUser({}));
+      }
+
+      const contentType = headers.get("content-type");
+      if (contentType?.includes("application/json")) {
+        return response.json();
+      }
+
+      return response.text();
+    },
+  }),
+);
