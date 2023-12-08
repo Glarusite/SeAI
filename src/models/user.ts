dfd16b2611d35cb8c6f681aa13d2d907b9781911
Viktor Mitev <vmmitev@gmail.com>
2023-12-08 21:55:11 +0200

    updated form models for API

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-import type { AuthRequest, UserRegisterRequest } from "@src/store";
+import type { AuthRequest, GetUserResponse, UserRegisterRequest } from "@src/store";
 
 export interface LoginFormData extends AuthRequest {}
 
@@ -6,8 +6,14 @@ export interface PasswordResetFormData extends AuthRequest {
   repeatPassword: string;
 }
 
-export interface RegisterFormData extends UserRegisterRequest {
+export interface RegisterFormData extends Pick<UserRegisterRequest, "email" | "firstName" | "lastName" | "password"> {
   repeatPassword: string;
+}
+
+export interface ProfileFormData extends Omit<GetUserResponse, "contractDuration" | "dateOfBirth" | "readinessDate"> {
+  contractDuration?: string;
+  dateOfBirth?: Date;
+  readinessDate?: Date;
 }
 
 export interface User {
