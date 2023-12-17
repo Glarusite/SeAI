ac54a11dc9a067ca2e1f8e86f419d4dc07aaf94a
Viktor Mitev <vmmitev@gmail.com>
2023-12-17 20:58:00 +0200

    added sorting by joiningDate for voyages

--- src/components/voyages/use-voyages.tsx
+++ src/components/voyages/use-voyages.tsx
@@ -1,7 +1,12 @@
+import { toLocalDate } from "@src/common/date";
 import { useAppSelector, useFindAllByUserQuery } from "@src/store";
 
 export function useVoyages() {
   const userId = useAppSelector(state => state.user.userId) || "";
   const { data = [], isLoading, error } = useFindAllByUserQuery(userId, { skip: !userId });
-  return { data: [...data].reverse(), isLoading, error };
+  const sortedData = [...data]
+    .filter(item => toLocalDate(item.joiningDate))
+    .sort((a, b) => toLocalDate(b.joiningDate)!.getTime() - toLocalDate(a.joiningDate)!.getTime());
+
+  return { data: sortedData, isLoading, error };
 }
