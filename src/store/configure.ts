fd89ca4eb17275d1fa67c6d5d0b47da4251b4b8f
Viktor Mitev <vmmitev@gmail.com>
2024-02-25 13:20:48 +0200

    removed extra code for store configuration and temp-fixed a weird type issue by using Array.concat which causes a code-style violation

--- src/store/configure.ts
+++ src/store/configure.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import type { StateFromReducersMapObject } from "@reduxjs/toolkit";
+import type { EnhancedStore, StateFromReducersMapObject } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
 import { safeJsonParse } from "@src/common/json";
 import { debounce } from "lodash";
@@ -13,7 +13,9 @@ export async function configureAppStore() {
   const store = configureStore({
     reducer,
     preloadedState,
-    middleware: getDefaultMiddleware => [...getDefaultMiddleware(), api.middleware],
+    // TODO: Check why type error occurs when using spread operator
+    // eslint-disable-next-line unicorn/prefer-spread
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
   });
 
   store.subscribe(
@@ -26,13 +28,5 @@ export async function configureAppStore() {
   return store;
 }
 
-function configureTypedStore(preloadedState: AppStoreState | undefined) {
-  return configureStore({
-    reducer,
-    preloadedState,
-    middleware: getDefaultMiddleware => [...getDefaultMiddleware(), api.middleware],
-  });
-}
-
-export type AppStore = ReturnType<typeof configureTypedStore>;
+export type AppStore = EnhancedStore;
 export type AppStoreState = StateFromReducersMapObject<typeof reducer>;
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import type { TypedUseSelectorHook } from "react-redux";
-import { useDispatch, useSelector, useStore } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import type { AppStore, AppStoreState } from "./configure";
 import { configureAppStore } from "./configure";
@@ -15,6 +15,5 @@ export function useCreateStore() {
   return store;
 }
 
-export const useAppStore = useStore as () => AppStore;
 export const useAppDispatch = useDispatch as () => AppStore["dispatch"];
 export const useAppSelector = useSelector as TypedUseSelectorHook<AppStoreState>;
