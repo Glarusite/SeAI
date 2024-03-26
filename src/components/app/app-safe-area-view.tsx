c51dfd28e3ce0e579b534d745cf9a6e99b74efcb
Viktor Mitev <vmmitev@gmail.com>
2024-03-26 13:02:28 +0200

    fixed scroll issue on web by disabling safe area view rendering when not needed

--- src/components/app/app-safe-area-view.tsx
+++ src/components/app/app-safe-area-view.tsx
@@ -1,12 +1,16 @@
 import { useAppOrientation } from "@src/common/hooks";
 import { Orientation } from "expo-screen-orientation";
 import type { PropsWithChildren } from "react";
+import { Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppSafeAreaView({ children }: PropsWithChildren) {
   const orientation = useAppOrientation();
 
-  if (orientation === Orientation.LANDSCAPE_LEFT || orientation === Orientation.LANDSCAPE_RIGHT) {
+  if (
+    Platform.OS !== "web" &&
+    (orientation === Orientation.LANDSCAPE_LEFT || orientation === Orientation.LANDSCAPE_RIGHT)
+  ) {
     return <SafeAreaView>{children}</SafeAreaView>;
   }
 
