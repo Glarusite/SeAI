62607c11925f2b5e1b8a8283031974f726e3afba
Viktor Mitev <vmmitev@gmail.com>
2024-04-10 20:20:19 +0300

    updated packages, started WIP on ESLint v9 flat config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
+/** @type {import("expo/metro-config").MetroConfig} */
 const MetroConfig = getDefaultConfig(__dirname, {});
 
 module.exports = MetroConfig;
