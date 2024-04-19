37a807125389c5e17524e774d1582747d64db05c
Viktor Mitev <vmmitev@gmail.com>
2024-04-19 10:33:36 +0300

    implemented parsing of error data

--- src/common/error.ts
+++ src/common/error.ts
@@ -1,12 +1,30 @@
 import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import type { ApiErrorResponse } from "@src/models";
 
 export function toErrorMessage(error: unknown) {
   const errorData =
     typeof error === "object" && "data" in (error as FetchBaseQueryError)
-      ? (error as FetchBaseQueryError).data
+      ? flattenErrorData(error as FetchBaseQueryError)
       : error instanceof Error
         ? error.message
         : error;
+
   const errorMessage = typeof errorData === "string" ? errorData : JSON.stringify(errorData);
+
   return errorMessage;
+}
+
+function flattenErrorData(error: FetchBaseQueryError) {
+  const { data } = error;
+  if (typeof data === "string") {
+    return data;
+  }
+
+  if (typeof data === "object") {
+    return "detail" in (data as ApiErrorResponse)
+      ? (data as ApiErrorResponse).detail
+      : "error" in (data as ApiErrorResponse)
+        ? (data as ApiErrorResponse).error
+        : error;
+  }
 }
--- src/models/app.ts
+++ src/models/app.ts
@@ -12,3 +12,12 @@ export type DropDownList<TValue extends string | number | undefined> = DropDownL
 export type DropDownListItem<TValue extends string | number | undefined> = Unboxed<DropDownPropsInterface["list"]> & {
   value?: TValue | "";
 };
+
+export interface ApiErrorResponse {
+  timestamp: string;
+  status: number;
+  error?: string;
+  detail?: string;
+  path?: string;
+  instance?: string;
+}
