b2d39e60920dc5bf231a344083d30fb1142e1f87
Viktor Mitev <vmmitev@gmail.com>
2024-06-10 00:35:50 +0300

    fixed router type-system issue

--- src/components/ui/buttons/link-button.tsx
+++ src/components/ui/buttons/link-button.tsx
@@ -1,14 +1,13 @@
-import type { LinkProps } from "expo-router";
 import { router } from "expo-router";
 import React, { useCallback } from "react";
 import type { ButtonProps } from "react-native-paper";
 import { Button } from "react-native-paper";
 
-export type LinkButtonProps<T> = Omit<ButtonProps, "onPress"> & {
-  href: LinkProps<T>["href"];
+export type LinkButtonProps = Omit<ButtonProps, "onPress"> & {
+  href: Parameters<(typeof router)["push"]>[0];
 };
 
-export default function LinkButton<T>({ href, children, mode = "outlined", ...props }: LinkButtonProps<T>) {
+export default function LinkButton({ href, children, mode = "outlined", ...props }: LinkButtonProps) {
   const handlePress = useCallback(() => {
     router.push(href);
   }, [href]);
